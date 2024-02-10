becddc9204e0e7ed5d9b899ebccc6415f6b0be20
Marwwagalal204 <Marwwagalal204@gmail.com>
2024-02-10 20:42:50 +0200

    fix consol

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -2,6 +2,13 @@
 
 import json
 from models.base_model import BaseModel
+from models.user import User
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.place import Place
+from models.review import Review
+from os.path import isfile
 
 
 class FileStorage:
@@ -31,3 +38,13 @@ class FileStorage:
                     self.__objects[key] = eval(class_name)(**value)
         except FileNotFoundError:
             pass
+
+FileStorage.classes = {
+        'BaseModel': BaseModel,
+        'User': User,
+        'State': State,
+        'City': City,
+        'Amenity': Amenity,
+        'Place': Place,
+        'Review': Review
+}
