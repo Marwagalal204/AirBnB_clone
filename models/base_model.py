5c04ef151b1ea0fa599071ad5641d532b2b80d07
Marwwagalal204 <Marwwagalal204@gmail.com>
2024-02-10 19:54:51 +0200

    second fix

--- models/base_model.py
+++ models/base_model.py
@@ -4,17 +4,19 @@ import models
 from datetime import datetime
 from uuid import uuid4
 
+
 class BaseModel:
     def __init__(self, *args, **kwargs):
         tform = "%Y-%m-%dT%H:%M:%S.%f"
         self.id = str(uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
         if kwargs:
             for key, value in kwargs.items():
                 if key != '__class__':
                     if key in ['created_at', 'updated_at']:
                         setattr(self, key, datetime.strptime(value, tform))
+        else:
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
             models.storage.new(self)
 
     def save(self):
@@ -27,9 +29,8 @@ class BaseModel:
         obj_dict['created_at'] = self.created_at.isoformat()
         obj_dict['updated_at'] = self.updated_at.isoformat()
         return obj_dict
-    
+
     def __str__(self):
         """Return the str representation of the BaseModel instance."""
         clname = self.__class__.__name__
         return "[{}] ({}) {}".format(clname, self.id, self.__dict__)
-
